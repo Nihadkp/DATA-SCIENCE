7860de17009e2f43aa94820e3b29af556401734f
Nihadkp <78895436+Nihadkp@users.noreply.github.com>
2022-01-04 11:47:50 +0530

    Update 5.py

--- Matrix/5.py
+++ Matrix/5.py
@@ -1,6 +1,6 @@
 import numpy as np
-matrix1 = np.array([[1,3,4],[3,6,2],[5,2,4]])
-matrix2 = np.array([[2,5,6],[5,3,2],[1,2,6]])
+matrix1 = np.array([[6,3,9],[4,7,5],[6,3,9]])
+matrix2 = np.array([[1,6,4],[8,2,4],[5,3,9]])
 
 print("Matrix addition:\n",matrix1 + matrix2)
 print("Matrix substraction:\n",matrix1 - matrix2)
