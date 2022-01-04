25cb893e707248d6138d7ab31aa61b2bd001403c
Nihadkp <78895436+Nihadkp@users.noreply.github.com>
2022-01-04 11:44:08 +0530

    Update 2.py

--- Matrix/2.py
+++ Matrix/2.py
@@ -1,5 +1,5 @@
 import numpy as np
-matrix = np.array([[2,4,6],[7,3,1],[4,8,2],[7,1,5]])
+matrix = np.array([[6,3,9],[4,7,5],[6,3,9],[5,1,2]])
 column,row = matrix.shape
 print("Number of rows:",row)
 print("Number of columns:",column)
